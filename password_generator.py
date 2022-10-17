25d10fc4a47f6c10e263e9d9d84bfbcf3f8cd90b
devyanic11 <104618068+devyanic11@users.noreply.github.com>
2022-10-17 15:45:38 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -6,7 +6,7 @@ class Password:
 
   def password_generator(self):
     """Generates a password"""
-    letters = ['sony','arcade','pune','goa','carleton', 'guleph', 'ottawa', 'america', 'Japan', 'Nihon', 'ichiban', 'kawai', 'tutsnuts', 'custard','melon','laxmi','aroma','aurora','atlantic','arctic','nato','nubik','nodem', 'angela']
+    letters = ['sony','arcade','pune','goa','carleton', 'guleph', 'ottawa', 'america', 'Japan', 'Nihon', 'ichiban', 'kawai', 'tutsnuts', 'custard','melon','aroma','aurora','atlantic','arctic','nato','nubik','nodem', 'angela']
     numbers = ['1234', '43557','2534','8544','7890','3498', '4592', '9832', '0532']
     symbols = ['#', '@','$',"!']
 
